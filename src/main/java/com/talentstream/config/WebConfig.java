075aecd6addaf5fd35bbf0a46b8f96cd52b5a5c0
madar1992 <96854154+madar1992@users.noreply.github.com>
2024-04-26 22:41:24 +0530

    Update WebConfig.java

--- src/main/java/com/talentstream/config/WebConfig.java
+++ src/main/java/com/talentstream/config/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig {
        //   config.addAllowedOrigin("https://d2csdrjtu6a4ys.cloudfront.net");
         config.addAllowedOrigin("https://jobs.bitlabs.in");
         config.addAllowedOrigin("http://localhost:3000");       
-        config.addAllowedOrigin("https://talent-stream-front-end-version2-eedekarunakar.vercel.app");
+        config.addAllowedOrigin("https://talent-stream-fe.vercel.app");
        
          config.addAllowedOrigin("https://talentstream-deployment-bucket.s3.amazonaws.com");
          config.setAllowedHeaders(Arrays.asList(
